2f5a2be98007fe1039bbe29d859f9d7f031e678f
gibeongideon <kipngeno.gibeon@gmail.com>
2021-02-07 00:07:58 +0300

    add tests

--- daruapp/settings.py
+++ daruapp/settings.py
@@ -143,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static')) #for production server
 
 
 AUTH_USER_MODEL = 'users.User'
