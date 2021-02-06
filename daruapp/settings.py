9841b9f431bb633ad74428d2317660d4fc998be0
gibeongideon <kipngeno.gibeon@gmail.com>
2021-02-06 13:52:52 +0300

    fix settins

--- daruapp/settings.py
+++ daruapp/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
+# from pathlib import Path
+import os
 from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -85,7 +87,7 @@ ASGI_APPLICATION = 'daruapp.asgi.application'  # AD
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), # BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -185,4 +187,26 @@ CELERY_BEAT_SCHEDULE = {
          'schedule': crontab(minute=[
              0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]),
         }
+}
+
+
+# log stuff to console
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+        'logfile': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR + "/../darulogfile",
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console', 'logfile']
+    },
 }
---
+++ functional_tests/base.py
@@ -0,0 +1,81 @@
+import os
+import time
+import io
+import pytest
+
+
+from django.conf import settings
+from django.core import management
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from .management.commands.create_session import (
+    create_pre_authenticated_session
+)
+
+
+MAX_WAIT = 10
+
+
+def reset_database_on_server(): # revisit
+    management.call_command('flush', verbosity=0, interactive=False)
+
+
+def create_session_on_server(username): # revisit
+    out = io.StringIO()
+    management.call_command('create_session', f'--username={username}', stdout=out)
+    return out.getvalue()
+
+
+def get_staging_server(): # revisit
+    server = os.environ.get('STAGING_SERVER')
+    return server
+
+
+def wait_to_load(fn):
+    def deco_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return deco_fn
+
+
+class FunctionalTestCase(StaticLiveServerTestCase):
+
+    def setUp(self):
+
+        self.live_server_url = get_staging_server()
+        if not self.live_server_url:
+            pytest.exit('Now running functional tests in only possible '
+                        'with staging server')
+
+        # Run browser
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+        super().tearDown()
+
+    def create_pre_authenticated_session(self, username):
+        self.staging_server = get_staging_server()
+        if self.staging_server:
+            # functional tests -> against real server
+            session_key = create_session_on_server(username) # revisit
+        else:
+            # unit tests -> against fake server
+            session_key = create_pre_authenticated_session(username)
+
+        # to set a cookie we need to first visit the domain.
+        # 404 pages load the quickest!
+        self.browser.get(self.live_server_url + "/404_no_such_url/")
+
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,
+            path='/',
+        ))
