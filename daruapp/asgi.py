9408baee4c93a1ef5142fd1cf9a937fd53cbdb12
gibeongideon <kipngeno.gibeon@gmail.com>
2021-02-08 23:43:25 +0300

    websock security add

--- daruapp/asgi.py
+++ daruapp/asgi.py
@@ -11,15 +11,18 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import daru_wheel.routing
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daruapp.settings")
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
+  "websocket": AllowedHostsOriginValidator(
+      AuthMiddlewareStack(
         URLRouter(
             daru_wheel.routing.websocket_urlpatterns
-        )
+            )
+        ),
     ),
-})
 
+})
