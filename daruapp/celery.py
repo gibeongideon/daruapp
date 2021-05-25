2ad5a63f76d13590ffac3c1b9edf0c9a67f587e1
gibeongideon <kipngeno.gibeon@gmail.com>
2021-05-25 21:41:41 +0300

    clean_up

--- daruapp/celery.py
+++ daruapp/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'daruapp.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'daruapp.settings')
 
 app = Celery('daruapp')
 
