f5a406bf7cbb754fddba97adcad5559ac0257159
gibeongideon <kipngeno.gibeon@gmail.com>
2021-06-03 00:27:09 +0300

    add_settup

--- daruapp/settings.py
+++ daruapp/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -168,9 +168,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / './static'#a os.path.abspath(os.path.join(BASE_DIR, '../static'))
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / './static'#a os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
 AUTH_USER_MODEL = 'users.User'
 
