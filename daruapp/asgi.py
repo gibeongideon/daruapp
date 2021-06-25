9c961b8073f4aba9588f3143e7b69e178cd30de0
gibeongideon <kipngeno.gibeon@gmail.com>
2021-06-25 23:55:43 +0300

    update_asgi_app

--- daruapp/asgi.py
+++ daruapp/asgi.py
@@ -6,47 +6,15 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
-
-# import os
-# import django
-# from channels.routing import get_default_application
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daruapp.settings")
-# django.setup()
-# application = get_default_application()
-
-
-# import os
-# # from channels.auth import AuthMiddlewareStack
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from django.daruapp.asgi import get_asgi_application
-# import daru_wheel.routing
-# from channels.security.websocket import AllowedHostsOriginValidator
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daruapp.settings")
-
-
-# application = ProtocolTypeRouter({
-#   "http": get_asgi_application(),
-#   "websocket": AllowedHostsOriginValidator(
-#     #   AuthMiddlewareStack(
-#         URLRouter(
-#             daru_wheel.routing.websocket_urlpatterns
-#             )
-#         # ),
-#     ),
-
-# })
-
 import os
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daruapp.settings")
-asgi_app = get_asgi_application()
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 import daru_wheel.routing
+
+asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
@@ -56,3 +24,17 @@ application = ProtocolTypeRouter(
         ),
     }
 )
+
+
+# application = ProtocolTypeRouter(
+#     {
+#         "http": asgi_app,
+#         "websocket": AllowedHostsOriginValidator(
+#             AuthMiddlewareStack(
+#                 URLRouter(
+#                     daru_wheel.routing.websocket_urlpatterns
+#                 )
+#             )
+#         ),
+#     }
+# )
